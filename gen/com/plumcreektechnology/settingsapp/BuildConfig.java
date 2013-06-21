b7108e6d739c9fffe87800fde85b5fb95bd8dac0
Nora Hayes <nhayes@oberlin.edu>
2013-06-21 13:47:30 -0400

    new settings test project

---
+++ gen/com/plumcreektechnology/settingsapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.plumcreektechnology.settingsapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
